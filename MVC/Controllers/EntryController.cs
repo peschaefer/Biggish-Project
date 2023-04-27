c9089a40604b5bfbabae2d51a7925f17b0c8b2f7
DrewH <adheiss@bsu.edu>
2023-04-27 16:15:03 -0400

    Add entries

--- MVC/Controllers/EntryController.cs
+++ MVC/Controllers/EntryController.cs
@@ -1,6 +1,76 @@
-namespace MVC.Controllers;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+using MVC.Repositories;
 
-public class EntryController
+namespace MVC.Controllers
 {
-    
+    public class EntryController : Controller
+    {
+        private readonly IEntryRepository _EntryRepository;
+
+        public EntryController(IEntryRepository EntryRepository)
+        {
+            _EntryRepository = EntryRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _EntryRepository.GetEntries());
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Id,Timestamp,Boarded,LeftBehind,Driver,Bus,Loop,Stop")] Entry Entry)
+        {
+            if (ModelState.IsValid)
+            {
+                await _EntryRepository.AddEntry(Entry);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return RedirectToAction(nameof(Index));
+
+        }
+        
+        [HttpPost, ActionName("Edit")]
+        public async Task<IActionResult> EditConfirmed(int id, [Bind("Id,Timestamp,Boarded,LeftBehind,Driver,Bus,Loop,Stop")] Entry Entry)
+        {
+            if (id != Entry.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _EntryRepository.UpdateEntry(Entry);
+                }
+                catch (Exception)
+                {
+                    return NotFound();
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+        
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int[] ids)
+        {
+            try
+            {
+                await _EntryRepository.DeleteEntries(ids);
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
 }
