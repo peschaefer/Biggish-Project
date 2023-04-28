4af6dee16d11109ad781bef993167ad348fb1470
Peyton Schaefer <peschaefer@bsu.edu>
2023-04-27 22:38:44 -0400

    Add route controller

---
+++ MVC/Controllers/RouteController.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+using MVC.Repositories;
+
+namespace MVC.Controllers
+{
+    public class RouteController : Controller
+    {
+        private readonly IRouteRepository _routeRepository;
+
+        public RouteController(IRouteRepository routeRepository)
+        {
+            _routeRepository = routeRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Route>>> GetAllRoutes()
+        {
+            var routes = await _routeRepository.GetRoutes();
+            return Ok(routes);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Route>> GetRoute(int id)
+        {
+            var route = await _routeRepository.GetRoute(id);
+            if (route == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(route);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Route>> CreateRoute([FromBody] Route route)
+        {
+            try
+            {
+                var createdRoute = await _routeRepository.AddRoute(route);
+                return Ok(createdRoute);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateRoute(int id, [FromBody] Route route)
+        {
+            if (id != route.Id)
+            {
+                return BadRequest("The ID specified in the route object does not match the route ID in the request URL.");
+            }
+
+            try
+            {
+                await _routeRepository.UpdateRoute(route);
+                return Ok(route);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteRoutes([FromBody] int[] ids)
+        {
+            try
+            {
+                var deletedRoutes = await _routeRepository.DeleteRoutes(ids);
+                return Ok(deletedRoutes);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
