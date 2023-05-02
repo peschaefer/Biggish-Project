196f85fc0ff77f0f7b7033b35aa590491e15d27a
DrewH <adheiss@bsu.edu>
2023-05-01 22:40:51 -0400

    fix map rendering

--- MVC/wwwroot/js/map.js
+++ MVC/wwwroot/js/map.js
@@ -1,21 +1,21 @@
+
 let map;
 
-async function initMap() {
+function initMap() {
   const position = { lat: -25.344, lng: 131.031 };
 
-  //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
-
-  map = new Map(document.getElementById("map"), {
+  map = new google.maps.Map(document.getElementById("map"), {
     zoom: 4,
     center: position,
-    mapId: "DEMO_MAP_ID",
   });
 
-  const marker = new AdvancedMarkerView({
+  const marker = new google.maps.Marker({
     map: map,
     position: position,
     title: "Uluru",
   });
 }
+
+window.addEventListener("load", initMap);
+
+
