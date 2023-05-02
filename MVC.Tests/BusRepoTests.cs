c50dcc23de2e6b608fe95b0b54e804e4b32199ba
Robert <ibsonnumber1@gmail.com>
2023-05-01 21:03:54 -0400

    Implement Add, Get, and Update tests to BusRepoTests. Also add skeleton for Delete.

--- MVC.Tests/BusRepoTests.cs
+++ MVC.Tests/BusRepoTests.cs
@@ -1,10 +1,117 @@
+using Microsoft.EntityFrameworkCore;
+using MVC.Models;
+using MVC.Repositories;
+using Moq;
+using System.Reflection.Metadata;
+using Castle.Components.DictionaryAdapter.Xml;
+using MVC.Controllers;
+using Microsoft.AspNetCore.Connections;
+using Microsoft.EntityFrameworkCore.Sqlite;
+using Microsoft.Data.Sqlite;
+
 namespace MVC.Tests;
 
 public class BusRepoTests
 {
-    [Fact]
-    public void Test1()
+    Bus bus1 = new Bus()
     {
-        Assert.True(true);
+        BusNumber = 1,
+        Id = 1
+    };
+
+    Bus bus2 = new Bus()
+    {
+        BusNumber = 2,
+        Id = 2
+    };
+
+    [Fact]
+    public async void TestAddBus()
+    {
+        var connection = new SqliteConnection("DataSource=:memory:");
+
+        var option = new DbContextOptionsBuilder<BigishProjContext>()
+            .UseSqlite(connection)
+            .Options;
+
+        var context = new BigishProjContext(option);
+
+        BusRepository busRepository = new BusRepository(context);
+
+        busRepository.AddBus(bus1);
+
+        var retrievedBus = context.Buses.FindAsync(1);
+
+        Assert.Equal(bus1.Id, retrievedBus.Result.Id);
+        Assert.Equal(bus1.BusNumber, retrievedBus.Result.BusNumber);
+
+        var deletion = busRepository.DeleteBuses(new int[1]);
+    }
+
+    [Fact]
+    public async void TestGetBus()
+    {
+        var connection = new SqliteConnection("DataSource=:memory:");
+
+        var option = new DbContextOptionsBuilder<BigishProjContext>()
+            .UseSqlite(connection)
+            .Options;
+
+        var context = new BigishProjContext(option);
+
+        BusRepository busRepository = new BusRepository(context);
+
+        busRepository.AddBus(bus1);
+
+        var retrievedBus = busRepository.GetBus(1);
+
+        Assert.Equal(bus1.Id, retrievedBus.Result.Id);
+        Assert.Equal(bus1.BusNumber, retrievedBus.Result.BusNumber);
+
+        var deletion = busRepository.DeleteBuses(new int[1]);
+    }
+
+    [Fact]
+    public async void TestUpdateBus()
+    {
+        var connection = new SqliteConnection("DataSource=:memory:");
+
+        var option = new DbContextOptionsBuilder<BigishProjContext>()
+            .UseSqlite(connection)
+            .Options;
+
+        var context = new BigishProjContext(option);
+
+        BusRepository busRepository = new BusRepository(context);
+
+        busRepository.AddBus(bus2);
+
+        Bus replacement = new Bus()
+        {
+            Id = 2,
+            BusNumber = 3
+        };
+
+        busRepository.UpdateBus(replacement);
+
+        var retrievedBus = busRepository.GetBus(2);
+
+        Assert.Equal(replacement.Id, retrievedBus.Result.Id);
+        Assert.Equal(replacement.BusNumber, retrievedBus.Result.BusNumber);
+
+        var deletion = busRepository.DeleteBuses(new int[2]);
+    }
+
+    [Fact]
+    public async void TestUpdateBusNotFound()
+    {
+        Assert.True(false);
+
+    }
+
+    [Fact]
+    public async void TestDeleteBuses()
+    {
+        Assert.True(false);
     }
 }
